5cd9e0e10544dd34e16aa4630c5fecc6c678de01
Gold Dimanche <golddimanche@yahoo.com>
2024-08-30 21:28:04 +0100

    Add files via upload

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,11 +1,8 @@
+import caesarart
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-direction = input("Type 'encrypt' to encrypt, type 'decode' to decode: ").lower()
-text = input("Type your message: ").lower()
-shift = int(input("What is the shift number: "))
 
-
-def caesar(directon):
-    if directon == 'encrypt':
+def caesar(encode_or_decode):
+    if encode_or_decode == 'encrypt':
         def encrypt(original_text, shift_amount):
             encode = ''
             for char in original_text:
@@ -15,21 +12,42 @@ def caesar(directon):
                     encode += alphabet[new_index]
                 else:
                     encode += char
-            print(f'Your Encoded word is {encode}')
+            print(f'Your encoded word is {encode}')
         encrypt(original_text=text, shift_amount=shift)
-    elif directon == 'decode':
+    elif encode_or_decode == 'decode':
         def decode (original_text, shift_amount):
             decoder = ''
             for char in original_text:
                 if char in alphabet:
-                    decrypt = (alphabet.index(char) - shift_amount)% len(alphabet)
-                    decoder += alphabet[decrypt]
+                    decode_position = (alphabet.index(char) - shift_amount)% len(alphabet)
+                    decoder += alphabet[decode_position]
                 else:
                     decoder += char
             print(f'Your decoded word is {decoder}')
         decode(original_text=text, shift_amount=shift)
-    else:
-        print('Please type the correct options')
 
 
-caesar(direction)
+
+
+print(caesarart.logo)
+start_over = True
+while start_over:
+    direction = input("Type 'encrypt' to encrypt, type 'decode' to decode: \n").lower()
+    text = input("Type your message: \n").lower()
+    shift = int(input("What is the shift number: \n"))
+
+    caesar(encode_or_decode=direction)
+
+    restart = input("Would you like to start over, 'yes' or 'no' ").lower()
+    if restart == 'no':
+        start_over = False
+        print('Goodbye')
+
+
+
+
+
+
+
+
+
